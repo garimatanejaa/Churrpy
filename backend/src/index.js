75ea01af64820cd02f377d71b0b3d887af6b327e
garimatanejaa <garmataneja@gmail.com>
2025-02-17 20:54:26 +0530

    changes done for final deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,11 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { app ,server} from "./lib/socket.js";
+import path from "path";
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
@@ -26,6 +28,13 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 server.listen(PORT, ()=>{
     console.log("server is running on port:"+PORT);
     connectDB();
