b9ee0c3d33f8d52331d210ed20ec1570f0d72b9f
Garima Taneja <113235690+garimatanejaa@users.noreply.github.com>
2025-02-17 21:06:34 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,9 +22,7 @@ app.use(cors({
     credentials : true
 }
 ));
-app.get("/", (req, res) => {
-    res.send("API is running...");
-});
+
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
