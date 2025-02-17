e15909c080568f9412a045d68b7f07e907aadfab
Garima Taneja <113235690+garimatanejaa@users.noreply.github.com>
2025-02-17 22:25:33 +0530

    Update ChatContainer.jsx

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useChatStore } from '../store/useChatStore';
 import ChatHeader from './ChatHeader';
 import MessageSkeleton from './skeletons/MessageSkeleton';
